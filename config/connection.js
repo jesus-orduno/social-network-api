d18f7f14cc397fb2b0ca060ff04e798326a28d87
jesus-orduno <jesus.orduno@gmail.com>
2022-12-03 16:53:19 -0700

    connection file

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,8 @@
+const { connect, connection } = require('mongoose');
 
+connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+module.exports = connection;
